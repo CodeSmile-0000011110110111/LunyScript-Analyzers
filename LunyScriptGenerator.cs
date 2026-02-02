c6f6487afc72f9483a0fdee208166c7112156e64
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-02 19:39:32 +0100

    add lunyscript analyzers project & dll, fix release build issues

---
+++ LunyScriptGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.IO;
+
+namespace LunyScript.Analyzers
+{
+	[Generator]
+	public sealed class LunyScriptGenerator : IIncrementalGenerator
+	{
+		public void Initialize(GeneratorInitializationContext context) {}
+		public void Initialize(IncrementalGeneratorInitializationContext context)
+		{
+			var provider = context.SyntaxProvider.CreateSyntaxProvider(
+				predicate: (node, _) => node is ClassDeclarationSyntax c /*&& c.Identifier.Text == "MyTargetClass"*/,
+				transform: (ctx, _) => ctx.Node.ToString() // Or extract specific data
+			);
+
+			context.RegisterSourceOutput(provider, (productionContext, className) =>
+			{
+				var message = $"{DateTime.Now}: LUNYSCRIPT ROSLYN: {className}";
+				File.AppendAllText("U:\\roslyntest.txt", message + "\n");
+
+				//productionContext.AddSource("GeneratedProperty.g.cs", $@"partial class MyTargetClass {{ public int MyInjectedProperty => 42; }}");
+			});
+		}
+	}
+}
